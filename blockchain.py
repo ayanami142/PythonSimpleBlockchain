d4f57dbdcfb779916a064da3ab9aadd262304f74
Vladyslav Ovcharenko <ovcharenko.vl@outlook.com>
2024-05-23 22:38:00 +0300

    Added pydoc and type hinting for Block and Blockchain classes

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,23 @@ import time
 
 
 class Block:
-    def __init__(self, index: int, previous_hash: str, transactions: str, timestamp=None, nonce=0):
+    def __init__(
+            self,
+            index: int,
+            previous_hash: str,
+            transactions: str,
+            timestamp=float | None,
+            nonce=int | None
+    ) -> None:
+        """
+        Initialize a new block.
+        :index: int - The position of the block in the blockchain.
+        :param previous_hash: str - The hash of the previous block in the chain.
+        :param transactions: str - The transactions included in the block.
+        :param timestamp: float | optional - The time the block was created. Defaults to the current time.
+        :param nonce: int | optional - The nonce used for the proof-of-work algorithm. Defaults to 0.
+        :return: None
+        """
         self.index = index
         self.previous_hash = previous_hash
         self.transactions = transactions
@@ -31,38 +47,73 @@ class Block:
 
 
 class BlockChain:
-    def __init__(self, difficulty=1):
+    def __init__(self, difficulty: int = 6) -> None:
+        """
+        Initializes a new instance of the BlockChain class.
+        :param difficulty (int, optional):
+             Difficulty level for mining blocks. Defaults to 6.
+                The higher the difficulty, the more computational
+                effort required to mine a block.
+        :returns: None
+        """
         self.chain = [BlockChain.create_genesis_block()]
         self.difficulty = difficulty
 
     @staticmethod
-    def create_genesis_block():
+    def create_genesis_block() -> Block:
+        """
+        Create a Block instance with 0 values as the first block in the chain
+        :return: Block instance
+        """
         return Block(0, "0", "Genesis Block")
 
-    def get_latest_block(self):
+    def get_latest_block(self) -> Block:
+        """
+        Get the latest block from the chain
+        :return: Latest Block instance
+        """
         return self.chain[-1]
 
-    def add_block(self, new_block: Block):
+    def add_block(self, new_block: Block) -> None:
+        """
+        Add new block to the chain and mine this before adding
+        :param new_block: Block instance
+        :return: None
+        """
         new_block.previous_hash = self.get_latest_block().hash
         new_block.mine_block(self.difficulty)
-        print('added new block = ', new_block)
         self.chain.append(new_block)
 
-    def is_chain_valid(self):
+    def is_chain_valid(self) -> bool:
+        """
+        return False if current block 's previous hash is not equal to previous,
+        or current block 's hash doesn't equal to calculated hash,
+        or mined hash doesn't equal to current block's hash
+        else return True
+        :return: bool
+        """
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
 
-            if current_block.hash != current_block.calculate_hash():
-                return False
-            if previous_block.hash != current_block.previous_hash:
-                return False
-            if not current_block.hash.startswith('0' * self.difficulty):
+            if (
+                    current_block.hash != current_block.calculate_hash() or
+                    previous_block.hash != current_block.previous_hash or
+                    not current_block.hash.startswith('0' * self.difficulty)
+            ):
                 return False
         return True
 
-    def calculate_index(self):
+    def calculate_index(self) -> int:
+        """
+        Get the latest block's index and increment it
+        :return: calculated index for the block
+        """
         return self.get_latest_block().index + 1
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Generate list of blocks in the chain and join them as a string
+        :return: joined list of strings with the blocks
+        """
         return "\n".join([str(block) for block in self.chain])
